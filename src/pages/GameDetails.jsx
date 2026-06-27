f807f3ab98e1667beae0fe6af1d8581fbd6ce68a
Ayan <ayankarak2005@gmail.com>
2026-06-27 16:58:23 +0530

    Dynamic UI making

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -1,11 +1,31 @@
 import { useParams } from "react-router-dom";
+import games from "../data/Games";
 
 function GameDetails() {
   const { gameName } = useParams();
 
+  const game = games.find(
+    (g) => g.slug === gameName
+  );
+
+  if (!game) {
+    return <h1>Game Not Found</h1>;
+  }
+
   return (
-    <div>
-      <h1>{gameName}</h1>
+    <div className="game-details">
+      <img
+        src={game.image}
+        alt={game.title}
+      />
+
+      <h1>{game.title}</h1>
+
+      <p>{game.description}</p>
+
+      <p>
+        Slug: {game.slug}
+      </p>
     </div>
   );
 }
