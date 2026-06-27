5c3816182a1481e91789708e6f7aba8b1aafe481
Ayan <ayankarak2005@gmail.com>
2026-06-27 16:16:19 +0530

    added react router part 1

--- src/component/GameCard.jsx
+++ src/component/GameCard.jsx
@@ -1,15 +1,22 @@
+import { Link } from "react-router-dom";
+
 function GameCard({ game }) {
   return (
     <div className="game-card">
+
       <img src={game.image} alt={game.title} />
 
       <h2>{game.title}</h2>
 
       <p>{game.description}</p>
 
-      <a href={game.link} className="play-btn">
+      <Link
+        className="play-btn"
+        to={`/games/${game.slug}`}
+      >
         ▶ Play Game
-      </a>
+      </Link>
+
     </div>
   );
 }
--- src/data/Games.js
+++ src/data/Games.js
@@ -13,13 +13,15 @@ const games = [
   {
     id: 1,
     title: "Rock Paper Scissors",
+    slug: "rock-paper-scissors",
     image: rock,
     description: "Interactive game with dynamic result logic.",
-    link: "/games/rock-paper-scissor"
+    link: "https://ayankarak.github.io/ROCK-PAPER-SCISSORS/"
   },
   {
     id: 2,
     title: "Tic Tac Toe",
+    slug: "tic-tac-toe",
     image: tic,
     description: "Dynamic winner detection with clean UI logic.",
     link: "https://ayankarak.github.io/TIC-TAC_TOE/"
@@ -27,6 +29,7 @@ const games = [
   {
     id: 3,
     title: "Snake Game",
+    slug: "snake-game",
     image: snake,
     description: "Classic snake game with smooth controls.",
     link: "https://ayankarak.github.io/MY-SNAKE-GAME/"
@@ -34,20 +37,23 @@ const games = [
   {
     id: 4,
     title: "Flappy Bird",
+    slug: "flappy-bird",
     image: flappy,
     description: "Challenging bird-flapping game with simple controls.",
-    link: "/games/flappy-bird"
+    link: "https://ayankarak.github.io/FLAPPY-BIRD/"
   },
     {
     id: 5,
     title: "Pong",
+    slug: "pong",
     image: pong,
     description: "Classic arcade game with simple controls.",
-    link: "/games/pong"
+    link: "https://ayankarak.github.io/PONG/"
     },
     {
         id: 6,
         title: "Tetris",
+        slug: "tetris",
         image: tetris,
         description: "Classic puzzle game with smooth controls.",
         link: "https://ayankarak.github.io/Tetris/"
@@ -55,6 +61,7 @@ const games = [
     {
         id: 7,
         title: "Car Racing",
+        slug: "car-racing",
         image: car,
         description: "Exciting car racing game with realistic physics.",
         link: "https://ayankarak.github.io/MY-CAR-GAMES/"
@@ -63,6 +70,7 @@ const games = [
     {
         id: 8,
         title: "Memory Match",
+        slug: "memory-match",
         image: match,
         description: "Challenging memory matching game with simple controls.",
         link: "https://ayankarak.github.io/MATCH-THE-IMAGES//"
@@ -70,16 +78,18 @@ const games = [
     {
         id: 9,
         title: "Cross Sum",
+        slug: "cross-sum",
         image: cross,
         description: "Grid-based puzzle where row and column sums must match the target numbers.",
-        link: "/games/cross-sum"
+        link: "https://ayankarak.github.io/CROSS-SUM/"
     },
     {
         id: 10,
         title: "Water Sorting",
+        slug: "water-sorting",
         image: water,
         description: "Challenging water sorting game with simple controls.",
-        link: "/games/water-sorting"
+        link: "https://ayankarak.github.io/WATER-SORTING/"
     }
 
   // baki games ekhane add korbo...
