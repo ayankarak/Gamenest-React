52fe922d2adfe98ae89740f14f035b1eeac53fd7
Ayan <ayankarak2005@gmail.com>
2026-06-27 17:21:41 +0530

    Search Feature added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,42 @@
+import { useState, useEffect } from "react";
 import Hero from "../component/Hero";
 import GameCard from "../component/GameCard";
 import games from "../data/Games";
 
 function Home() {
-  return (
-    <>
-      <Hero />
+    const [search,setSearch] = useState("");
 
-      <section className="games-container" id="games">
-        {games.map((game) => (
-          <GameCard key={game.id} game={game} />
-        ))}
-      </section>
-    </>
+    const filteredGames = games.filter((game) =>
+    game.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+   return (
+        <>
+            <Hero />
+
+            {/* Search Bar */}
+            <div className="search-container">
+                <input
+                type="text"
+                placeholder="Search your favorite game..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                />
+            </div>
+
+            {/* Game Cards */}
+            <section className="games-container" id="games">
+                {filteredGames.length > 0 ? (
+                filteredGames.map((game) => (
+                    <GameCard key={game.id} game={game} />
+                ))
+                ) : (
+                <h2 className="no-games">
+                    No games found 😔
+                </h2>
+                )}
+            </section>
+        </>
   );
 }
 
