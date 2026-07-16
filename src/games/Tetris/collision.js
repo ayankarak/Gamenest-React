8c2dc61c52142c7ef486aaaa6679c57170d6f265
Ayan <ayankarak2005@gmail.com>
2026-07-16 15:22:53 +0530

    collision logic added

---
+++ src/games/Tetris/collision.js
@@ -0,0 +1,46 @@
+import {
+    ROWS,
+    COLS
+} from "./constant";
+
+export const isCollision = (
+    board,
+    piece
+) => {
+
+    for (let y = 0; y < piece.shape.length; y++) {
+
+        for (let x = 0; x < piece.shape[y].length; x++) {
+
+            if (piece.shape[y][x] === 0)
+                continue;
+
+            const newX = piece.x + x;
+            const newY = piece.y + y;
+
+            // Left Right Bottom
+
+            if (
+                newX < 0 ||
+                newX >= COLS ||
+                newY >= ROWS
+            ) {
+                return true;
+            }
+
+            // Existing Block
+
+            if (
+                newY >= 0 &&
+                board[newY][newX] !== 0
+            ) {
+                return true;
+            }
+
+        }
+
+    }
+
+    return false;
+
+};
